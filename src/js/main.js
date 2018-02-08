a0a6600320ea156e5bb35bfafcd82fd3a4b5c537
Maxim Drachev <drachyov.maxim@gmail.com>
2018-02-08 18:49:13 +0300

    update

--- src/js/main.js
+++ src/js/main.js
@@ -15,6 +15,13 @@ $(document).ready(function(){
     return false;
   });
 
+  $('.footer-item.back a[href*="#"]').click(function() {
+    var target = $(this.hash);
+    if (target.length == 0){ return true; }
+    $root.animate({scrollTop: target.offset().top }, 500);
+    return false;
+  });
+
   $('.project-slider-mobile').slick();
 
   $('.events-slider').slick();
@@ -30,9 +37,11 @@ function showProjectAbout() {
     var el = $(this);
     var container = el.closest('.project-item');
     var about = container.find('.project-about');
+    var footer = $('.footer');
     about.removeClass('hidden');
     container.css('margin-bottom', 0);
     container.nextAll().addClass('hidden');
+    footer.addClass('hidden');
     event.preventDefault();
   });
 
@@ -40,12 +49,47 @@ function showProjectAbout() {
     var el = $(this);
     var container = el.closest('.project-item');
     var about = container.find('.project-about');
+    var footer = $('.footer');
     about.addClass('hidden');
     container.css('margin-bottom', '');
     container.nextAll().removeClass('hidden');
+    footer.removeClass('hidden');
     event.preventDefault();
   });
 }
+
+$(document).ready(function(){
+  var el = $('#main');
+  var menuTrigger = $('#menu-trigger');
+  var mobileMenu = $('.mobile-menu');
+  var $root = $('html, body');
+
+  menuTrigger.click(function(event) {
+    el.toggleClass("menu-opened");
+    setTimeout( function() {
+      mobileMenu.toggleClass("opened");
+    } , 50);
+  });
+
+  var trigger = $('.trigger');
+  var url = window.location.pathname.replace(/^\/+/g, '');
+  trigger.click(function(event) {
+    url = $(this).attr('href');
+    $('.section').removeClass('active');
+    if (url=="#projects") {
+      url = "#startpage";
+      $(url).addClass('active');
+      setTimeout( function() {
+        $root.animate({scrollTop: $('#projects').offset().top - 42 }, 0);
+      } , 1);
+    } else {
+      $(url).addClass('active');
+    }
+    el.removeClass("menu-opened");
+    mobileMenu.removeClass("opened");
+    event.preventDefault();
+  });
+});
 
 // document.addEventListener("DOMContentLoaded", function() {
 //   var url = window.location.pathname.replace(/^\/+/g, ''),
