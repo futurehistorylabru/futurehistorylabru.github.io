42705a261326cc4b418bda718ba8e4d4c69dfecf
Maxim Drachev <drachyov.maxim@gmail.com>
2018-02-09 09:21:02 +0300

    team added

--- src/js/main.js
+++ src/js/main.js
@@ -27,6 +27,7 @@ $(document).ready(function(){
   $('.events-slider').slick();
 
   showProjectAbout();
+  showTeamAbout();
 });
 
 function showProjectAbout() {
@@ -49,6 +50,35 @@ function showProjectAbout() {
     var el = $(this);
     var container = el.closest('.project-item');
     var about = container.find('.project-about');
+    var footer = $('.footer');
+    about.addClass('hidden');
+    container.css('margin-bottom', '');
+    container.nextAll().removeClass('hidden');
+    footer.removeClass('hidden');
+    event.preventDefault();
+  });
+}
+
+function showTeamAbout() {
+  var aboutTeamLink = $('.team-show-more');
+  var closeTeamLink = $('.team-about-close-link');
+
+  aboutTeamLink.click(function(event) {
+    var el = $(this);
+    var container = el.closest('.team-item');
+    var about = container.find('.team-about');
+    var footer = $('.footer');
+    about.removeClass('hidden');
+    container.css('margin-bottom', 0);
+    container.nextAll().addClass('hidden');
+    footer.addClass('hidden');
+    event.preventDefault();
+  });
+
+  closeTeamLink.click(function(event) {
+    var el = $(this);
+    var container = el.closest('.team-item');
+    var about = container.find('.team-about');
     var footer = $('.footer');
     about.addClass('hidden');
     container.css('margin-bottom', '');
@@ -89,58 +119,11 @@ $(document).ready(function(){
     mobileMenu.removeClass("opened");
     event.preventDefault();
   });
+  var highestBox = 0;
+  $('.events-slider-item').each(function(){
+    if($(this).height() > highestBox) {
+      highestBox = $(this).height(); 
+    }
+  });  
+  $('.events-slider-item').height(highestBox);
 });
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   var url = window.location.pathname.replace(/^\/+/g, ''),
-//       windowWidth = window.innerWidth,
-//       menuTrigger = document.getElementById("menu-trigger"),
-//       main = document.getElementById("main");
-
-//   if (menuTrigger) {
-//     menuTrigger.addEventListener("click", function() {
-//       if (!main.classList.contains("menu-opened")) {
-//         main.classList.add("menu-opened");
-//       } else {
-//         main.classList.remove("menu-opened");
-//       }
-//     });
-//   }
-
-//   initTabs(url);
-// });
-
-// function initTabs(url) {
-//   var tabActive = document.getElementById(url),
-//       tabTrigger = document.querySelectorAll('.trigger');
-
-//   if (tabActive) {
-//     tabActive.classList.add('active');
-//   } else {
-//     document.getElementById('intro').classList.add('active');
-//   }
-
-//   for(var i = 0; i < tabTrigger.length ; i++) {
-//     tabTrigger[i].addEventListener("click", function(e) {
-//       openTab(this, url);
-//       e.preventDefault();
-//     });
-//   }
-// }
-
-// function openTab(elem, url) {
-//   var id = elem.getAttribute('data-tab'),
-//       main = document.getElementById("main");
-//   main.classList.remove("menu-opened");
-//   closeTab();
-//   document.getElementById(id).classList.add('active');
-//   window.history.pushState({url: "" + id + ""}, id, id);
-// }
-
-// function closeTab() {
-//   var menuElements = document.querySelectorAll('.trigger');
-//   for(var i = 0; i < menuElements.length ; i++) {
-//     var id = menuElements[i].getAttribute('data-tab');
-//     document.getElementById(id).classList.remove('active');
-//   }
-// }
